89e67d0b1c900326f5c68c61f14989153e376bd4
陈铁柱 <997610399@qq.com>
2022-04-09 15:45:43 +0800

    0409

---
+++ hash.py
@@ -0,0 +1,25 @@
+import json
+from Crypto import Random
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_v1_5 as Cipher_pkcs1_v1_5
+import base64
+
+random_gen = Random.new().read
+print(random_gen)
+rsa = RSA.generate(2048, random_gen)  # 获取秘钥对
+private_pem = rsa.exportKey()  # 获取私钥
+with open("private.pem", "wb") as f:
+    f.write(private_pem)
+public_pem = rsa.publickey().exportKey()  # 获取公钥
+with open("public.pem", "wb") as f:
+    f.write(public_pem)
+
+message = {"orderId": 9252678862671456643, "money": 50, "type": 2, "style": 241, 'userName': "241_liu924Wxy2"}
+message = json.dumps(message)
+rsaKey = RSA.importKey(open("public.pem").read())
+cipher = Cipher_pkcs1_v1_5.new(rsaKey)  # 创建用于执行pkcs1_v1_5加密或解密的密码
+cipher_text = base64.b64encode(cipher.encrypt(message.encode('utf-8')))
+print(cipher_text.decode('utf-8'))
+
+# private_pem = rsa.exportKey() with open('private.pem', 'wb') as f: f.write(private_pem)
+# public_pem = rsa.publickey().exportKey()with open('public.pem', 'wb') as f: f.write(public_pem)
